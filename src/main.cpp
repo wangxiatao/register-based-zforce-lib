fc1c2ca246c046a4256783ad0bf62bc64edff46b
tao <wangxiatao@gmail.com>
2020-03-31 17:25:59 +0200

    make it more of a practical case

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,17 @@ typedef struct SensorReg
 namespace SensorHelper
 {
 const sensor_reg_t reg_R_Status = 0x00;
-const sensor_reg_t reg_R_Bootcomplete = 0x08;
-const sensor_reg_t reg_RW_Enable = 0x11;
-const sensor_reg_t reg_RW_Frequency = 0x13;
-const sensor_reg_t reg_RW_Area = 0x14;
-const sensor_reg_t reg_R_Touch = 0x20; // from 0x20 to 0x2F
+const sensor_reg_t reg_R_Bootcomplete = 0x01;
+const sensor_reg_t reg_RW_Enable = 0x02;
+const sensor_reg_t reg_RW_Frequency = 0x03;
+const sensor_reg_t reg_RW_Area = 0x06;
+const sensor_reg_t reg_R_Touch = 0x0A; // from 0x0A to 0x1A
 
-#define MAX_REGS 20
+#define TOUCH_BUFFER_SIZE 4 // must not exeed 4
+uint8_t nTouches = 0;
+#define MAX_REGS (reg_R_Touch + TOUCH_BUFFER_SIZE * 4)
 sensor_val_t regs[MAX_REGS];
 
-#define TOUCH_BUFFER_SIZE 4 // must not exeed 4 
-TouchData touches[TOUCH_BUFFER_SIZE];
-uint8_t nTouches = 0;
 
 volatile bool newTouchDataFlag = false;
 void dataReadyISR() { newTouchDataFlag = true; }
@@ -32,66 +31,59 @@ bool isDataReady() { return digitalRead(PIN_NN_DR) == HIGH; }
 bool begin();
 void config(uint8_t index = 0);
 uint8_t updateTouch();
-bool writeReg(sensor_reg_t addr, sensor_val_t val);
+bool writeReg(sensor_reg_t addr, sensor_val_t val, bool sendToZforce = true);
 bool writeReg(SensorReg reg) { return writeReg(reg.addr, reg.val); }
 sensor_val_t readReg(sensor_reg_t addr);
 bool readReg(SensorReg_t &reg);
+bool sendAndGetFromZforce(sensor_reg_t addr);
+void mapTouchdataToRegs(TouchData *touch, uint8_t index);
+void getTouchdataFromRegs(TouchData &touch, uint8_t index);
 void printTouchMessage();
+void printOneReg(sensor_reg_t addr) { Serial << "regs[" << addr << "] = " << _HEX(regs[addr]) << endl; }
+void printRegs();
 } // namespace SensorHelper
 
-bool SensorHelper::writeReg(sensor_reg_t addr, sensor_val_t val)
+bool SensorHelper::writeReg(sensor_reg_t addr, sensor_val_t val, bool sendToZforce)
 {
-    switch (addr)
-    {
-    case reg_RW_Enable:
-        zforce.Enable(val);
-        regs[reg_RW_Enable] = val;
-        break;
-    case reg_RW_Frequency:
-        break;
-    case reg_RW_Area:
-        break;
-    default:
+    if (addr >= MAX_REGS)
         return false;
+    regs[addr] = val;
+
+    if(sendToZforce)
+        return sendAndGetFromZforce(addr);
+    else
+        return true;
+}
+
+bool SensorHelper::sendAndGetFromZforce(sensor_reg_t addr)
+{
+    if (addr == reg_RW_Enable)
+        zforce.Enable(regs[addr]);
+    else if (addr == reg_RW_Frequency)
+    {
+        /* code */
+    }
+    else if (addr == reg_RW_Area)
+    {
+        zforce.TouchActiveArea(regs[addr], regs[addr + 1],
+                               regs[addr + 2], regs[addr + 3]);
     }
 
     while (!isDataReady())
         ;
     Message *msg = zforce.GetMessage();
+    if(msg == NULL)
+        return false;
     zforce.DestroyMessage(msg);
     return true;
 }
 
 sensor_val_t SensorHelper::readReg(sensor_reg_t addr)
 {
-    sensor_val_t val = 0;
+    if (addr >= MAX_REGS)
+        return 0;
 
-    switch (addr)
-    {
-    case reg_R_Status:
-    case reg_RW_Enable:
-    case reg_RW_Frequency:
-    case reg_RW_Area:
-        val = regs[addr];
-        break;
-    }
-
-    if(addr >= reg_R_Touch && addr <= reg_R_Touch + 0x0F)
-    // touch regs range from 0x20 to 0x2F
-    {
-        uint8_t nTp = (addr - reg_R_Touch) / 4;
-        uint8_t nParam = (addr - reg_R_Touch) % 4;
-        if (nParam == 0)
-            val = touches[nTp].x;
-        else if (nParam == 1)
-            val = touches[nTp].y;
-        else if (nParam == 2)
-            val = touches[nTp].id;
-        else
-            val = touches[nTp].event;
-    }
-
-    return val;
+    return regs[addr];
 }
 
 bool SensorHelper::begin()
@@ -101,7 +93,10 @@ bool SensorHelper::begin()
     if (isDataReady())
     {
         Message *msg = zforce.GetMessage();
-        Serial << (int)msg->type << endl;
+        if (msg->type == MessageType::BOOTCOMPLETETYPE)
+            Serial << "Sensor connected" << endl;
+        else
+            Serial << "Unexpected senosr message" << endl;
         zforce.DestroyMessage(msg);
     }
     return true;
@@ -111,7 +106,29 @@ void SensorHelper::config(uint8_t index)
 {
     detachInterrupt(digitalPinToInterrupt(PIN_NN_DR));
     writeReg(reg_RW_Enable, true);
+    // writeReg(reg_RW_Area + 3, 100, false);
+    // writeReg(reg_RW_Area + 2, 100, false);
+    // writeReg(reg_RW_Area + 1, 0, false);
+    // writeReg(reg_RW_Area, 0);
     attachInterrupt(digitalPinToInterrupt(PIN_NN_DR), dataReadyISR, RISING);
+}
+
+void SensorHelper::mapTouchdataToRegs(TouchData *touch, uint8_t index)
+{
+    auto i = index;
+    regs[reg_R_Touch + i * 4 + 0] = touch->x;
+    regs[reg_R_Touch + i * 4 + 1] = touch->y;
+    regs[reg_R_Touch + i * 4 + 2] = touch->id;
+    regs[reg_R_Touch + i * 4 + 3] = touch->event;
+}
+
+void SensorHelper::getTouchdataFromRegs(TouchData &touch, uint8_t index)
+{
+    auto i = index;
+    touch.x = regs[reg_R_Touch + i * 4 + 0];
+    touch.y = regs[reg_R_Touch + i * 4 + 1];
+    touch.id = regs[reg_R_Touch + i * 4 + 2];
+    touch.event = (TouchEvent)regs[reg_R_Touch + i * 4 + 3];
 }
 
 uint8_t SensorHelper::updateTouch()
@@ -128,11 +145,10 @@ uint8_t SensorHelper::updateTouch()
         if (touch->type == MessageType::TOUCHTYPE)
         {
             auto size = ((TouchMessage *)touch)->touchCount;
-            size = size >= TOUCH_BUFFER_SIZE ? TOUCH_BUFFER_SIZE : size;
-            nTouches = size;
-            for (uint8_t i = 0; i < size; i++)
+            nTouches = size >= TOUCH_BUFFER_SIZE ? TOUCH_BUFFER_SIZE : size;
+            for (uint8_t i = 0; i < nTouches; i++)
             {
-                memcpy(touches + i, &((TouchMessage *)touch)->touchData[i], sizeof(TouchData));
+                mapTouchdataToRegs(&((TouchMessage *)touch)->touchData[i], i);
             }
             zforce.DestroyMessage(touch);
             return nTouches;
@@ -144,11 +160,21 @@ uint8_t SensorHelper::updateTouch()
 
 void SensorHelper::printTouchMessage()
 {
+    TouchData touch;
     for (size_t i = 0; i < nTouches; i++)
     {
-        Serial << "(" << touches[i].id << ")\t[" << touches[i].x << ", " << touches[i].y << "]\t(" << touches[i].event << ")\n";
+        getTouchdataFromRegs(touch, i);
+        Serial << "(" << i << "/" << nTouches << ")\t["
+               << touch.x << ", " << touch.y << "]\t("
+               << touch.event << "/" << touch.id << ")\n";
     }
     Serial << endl;
+}
+
+void SensorHelper::printRegs()
+{
+    for (size_t i = 0; i < MAX_REGS; i++)
+        printOneReg(i);
 }
 
 void setup()
@@ -158,7 +184,8 @@ void setup()
         ;
     SensorHelper::begin();
     SensorHelper::config();
-    Serial.println("zforce start\n");
+    SensorHelper::printRegs();
+    Serial.println("Sensor configured\n");
 }
 
 void loop()
@@ -166,11 +193,11 @@ void loop()
     auto result = SensorHelper::updateTouch();
     if (result > 0)
     {
-        // SensorHelper::printTouchMessage();
-        for (size_t i = 0; i < 4; i++)
+        SensorHelper::printTouchMessage();
+
+        if (SensorHelper::readReg(SensorHelper::reg_R_Touch + 3) == 2)
         {
-            Serial << SensorHelper::readReg(SensorHelper::reg_R_Touch + i) << "\t";
+            SensorHelper::printRegs();
         }
-        Serial << "\n\n";
     }
 }
